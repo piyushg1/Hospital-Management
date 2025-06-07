a71b70c2a781fe4d366e42cadaf293f490d0fdc1
Piyush <piyushat25@gmail.com>
2025-06-07 16:34:33 +0530

    changing datatype of id from string to Long

---
+++ src/main/java/com/practice/hospital_management/HospitalManagementApplication.java
@@ -0,0 +1,21 @@
+package com.practice.hospital_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HospitalManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
